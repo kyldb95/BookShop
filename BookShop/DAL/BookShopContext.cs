b31330fb8e4a449724c2692286d1665f597d3960
Kyle Bartlett <kyle.d.bartlett95@gmail.com>
2021-03-27 14:41:30 +0000

    Adding Controller, Views and Model.

---
+++ BookShop/DAL/BookShopContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookShop.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BookShop.DAL
+{
+    public class BookShopContext : DbContext
+    {
+        public BookShopContext() : base("BookShopContext")
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
